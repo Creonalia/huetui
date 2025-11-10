f715f84d9b375cad10cb6728ff028133f8a6a024
Creonalia <52385967+Creonalia@users.noreply.github.com>
2025-11-10 03:52:37 +0000

    feat: add footer with keybinds and hsl values
    feat: add bottom border line to footer
    
    a
    
    a

--- src/main.rs
+++ src/main.rs
@@ -4,6 +4,7 @@ use ratatui::{
     DefaultTerminal, Frame,
     palette::{ClampAssign, Hsl},
     style::{Color, Style},
+    text::Line,
     widgets::{Block, Borders},
 };
 
@@ -18,6 +19,7 @@ fn main() -> Result<()> {
 #[derive(Debug)]
 pub struct App {
     hsl: Hsl,
+    show_footer: bool,
     exit: bool,
 }
 
@@ -25,6 +27,7 @@ impl Default for App {
     fn default() -> Self {
         Self {
             hsl: Hsl::new(0.0, 0.0, 1.0),
+            show_footer: true,
             exit: false,
         }
     }
@@ -40,12 +43,27 @@ impl App {
     }
 
     fn draw(&self, frame: &mut Frame) {
-        frame.render_widget(
-            Block::new()
-                .borders(Borders::empty())
-                .style(Style::default().bg(Color::from_hsl(self.hsl))),
-            frame.area(),
-        );
+        let color = Color::from_hsl(self.hsl);
+        let mut color_block = Block::new().style(Style::default().bg(color));
+        if self.show_footer {
+            color_block = color_block
+                .borders(Borders::BOTTOM)
+                .border_style(Style::reset().fg(color))
+                .title_style(Style::reset())
+                .title_bottom(
+                    Line::from(format![
+                        " Hue (-h/+l): {} | Saturation (-u/+i): {:.2} | Lightness (-j/+k) {:.2} ",
+                        self.hsl.hue.into_degrees(),
+                        self.hsl.saturation,
+                        self.hsl.lightness,
+                    ])
+                    .centered(),
+                )
+                .title_bottom(Line::from(" Footer (?) ").left_aligned())
+                .title_bottom(Line::from(" Quit (q) ").right_aligned());
+        }
+
+        frame.render_widget(color_block, frame.area());
     }
 
     fn handle_events(&mut self) -> Result<()> {
@@ -53,6 +71,7 @@ impl App {
             Event::Key(key_event) if key_event.kind == KeyEventKind::Press => {
                 match key_event.code {
                     KeyCode::Char('q') => self.exit = true,
+                    KeyCode::Char('?') => self.show_footer = !self.show_footer,
 
                     KeyCode::Char('j') => self.hsl.lightness -= 0.05,
                     KeyCode::Char('J') => self.hsl.lightness -= 0.01,
